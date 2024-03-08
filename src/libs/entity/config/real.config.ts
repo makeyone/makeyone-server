8bfe5f2d4aeecd3896f4561c48011c8ec8ac5a01
Kim LeeJun <hwanam1111@naver.com>
2024-03-08 14:56:43 +0900

    🔨 fix: typeorm 커스텀 로그

--- src/libs/entity/config/real.config.ts
+++ src/libs/entity/config/real.config.ts
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import { NamingStrategyInterface } from 'typeorm/naming-strategy/NamingStrategyInterface';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
+import { TypeormCustomLogger } from '@src/libs/entity/config/custom-logger.config';
+
 dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
 }).parsed;
@@ -16,7 +18,8 @@ export const realTypeormConfig: TypeOrmModuleOptions = {
   database: process.env.DB_NAME,
   synchronize: process.env.NODE_ENV !== 'prod',
   bigNumberStrings: false,
-  logging: process.env.NODE_ENV === 'dev' && ['query', 'error'],
+  // logging: process.env.NODE_ENV === 'dev' && ['query', 'error'],
+  logger: new TypeormCustomLogger(true),
   charset: 'utf8mb4',
   entities: ['dist/**/!(core).entity{.ts,.js}'],
   namingStrategy: new SnakeNamingStrategy() as unknown as NamingStrategyInterface,
