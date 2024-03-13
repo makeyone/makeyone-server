de2771e692970ed94a8942a8aa5d2540784c9e0e
Kim LeeJun <hwanam1111@naver.com>
2024-03-13 14:37:27 +0900

    ✨ feat: 하우징 WINDOW KEY 레이아웃에 CWKL 추가

--- src/libs/entity/domain/post/enums/KeyboardHousingWindowKeyLayout.enum.ts
+++ src/libs/entity/domain/post/enums/KeyboardHousingWindowKeyLayout.enum.ts
@@ -1,7 +1,7 @@
 import { Enum, EnumConstNames, EnumType } from 'ts-jenum';
 
-type EnumCode = 'WK' | 'WKL' | 'HHKB' | 'None';
-type EnumName = '윈키' | '윈키리스' | 'HHKB' | '없음';
+type EnumCode = 'WK' | 'WKL' | 'CWKL' | 'HHKB' | 'None';
+type EnumName = '윈키' | '윈키리스' | 'C윈키리스' | 'HHKB' | '없음';
 type EnumTypeGeneric = KeyboardHousingWindowKeyLayout<EnumCode, EnumName>;
 
 @Enum<EnumTypeGeneric>('code')
@@ -11,6 +11,7 @@ export class KeyboardHousingWindowKeyLayout<
 > extends EnumType<EnumTypeGeneric>() {
   static readonly None = new KeyboardHousingWindowKeyLayout('None', '없음');
   static readonly WK = new KeyboardHousingWindowKeyLayout('WK', '윈키');
+  static readonly CWKL = new KeyboardHousingWindowKeyLayout('CWKL', 'C윈키리스');
   static readonly WKL = new KeyboardHousingWindowKeyLayout('WKL', '윈키리스');
   static readonly HHKB = new KeyboardHousingWindowKeyLayout('HHKB', 'HHKB');
 
