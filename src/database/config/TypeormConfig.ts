cbfb35ed4ebbc51db3756b2462b1f8389cbf15f6
Kim LeeJun <hwanam1111@naver.com>
2025-03-11 23:10:12 +0900

    🔨 fix: entity 직접 명시

--- src/database/config/TypeormConfig.ts
+++ src/database/config/TypeormConfig.ts
@@ -6,6 +6,19 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { addTransactionalDataSource } from 'typeorm-transactional';
 
 import { TypeormCustomLogger } from '@src/database/config/TypeormCustomLogger';
+import { PostEntity } from '@src/database/entity/Post/Post.entity';
+import { PostFoamEntity } from '@src/database/entity/Post/PostFoam.entity';
+import { PostHousingEntity } from '@src/database/entity/Post/PostHousing.entity';
+import { PostImageEntity } from '@src/database/entity/Post/PostImage.entity';
+import { PostKeyboardDefinitionEntity } from '@src/database/entity/Post/PostKeyboardDefinition.entity';
+import { PostKeycapEntity } from '@src/database/entity/Post/PostKeycap.entity';
+import { PostPlateEntity } from '@src/database/entity/Post/PostPlate.entity';
+import { PostPrintedCircuitBoardEntity } from '@src/database/entity/Post/PostPrintedCircuitBoard.entity';
+import { PostStabilizerEntity } from '@src/database/entity/Post/PostStabilizer.entity';
+import { PostSwitchEntity } from '@src/database/entity/Post/PostSwitch.entity';
+import { PostVideoEntity } from '@src/database/entity/Post/PostVideo.entity';
+import { UserEntity } from '@src/database/entity/User/User.entity';
+import { UserTokenEntity } from '@src/database/entity/User/UserToken.entity';
 
 dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
@@ -24,7 +37,21 @@ const typeormConfig: TypeOrmModuleAsyncOptions = {
       bigNumberStrings: false,
       logger: new TypeormCustomLogger(true),
       charset: 'utf8mb4',
-      entities: ['dist/**/!(Base).entity{.ts,.js}'],
+      entities: [
+        UserEntity,
+        UserTokenEntity,
+        PostEntity,
+        PostFoamEntity,
+        PostHousingEntity,
+        PostImageEntity,
+        PostKeyboardDefinitionEntity,
+        PostKeycapEntity,
+        PostPlateEntity,
+        PostPrintedCircuitBoardEntity,
+        PostStabilizerEntity,
+        PostSwitchEntity,
+        PostVideoEntity,
+      ],
       namingStrategy: new SnakeNamingStrategy() as unknown as NamingStrategyInterface,
     };
   },
